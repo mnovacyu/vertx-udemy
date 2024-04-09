6a31a05cac49e4dc0141031a1377c59aaf368941
Michael Novac-Yu <michael.novacyu@gmail.com>
2024-04-08 18:50:08 -0700

    set up repo with verticle object and verticle scaling

---
+++ vertx-starter/src/main/java/com/mnovacyu/vertx/vertx_starter/verticles/VerticleAB.java
@@ -0,0 +1,19 @@
+package com.mnovacyu.vertx.vertx_starter.verticles;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class VerticleAB extends AbstractVerticle {
+
+  @Override
+  public void start(final Promise<Void> startPromise) throws Exception {
+    System.out.println("Start " + getClass().getName());
+    startPromise.complete();
+  }
+
+  @Override
+  public void stop(final Promise<Void> stopPromise) throws Exception {
+    System.out.println("Stop " + getClass().getName());
+    stopPromise.complete();
+  }
+}
